1f17ecbcf1f4461faf973135a722f78e5818e5d5
Deivid Veloso <veloso.deivid@gmail.com>
2017-05-30 09:13:28 -0300

    props to format text

--- appStyle/index.android.js
+++ appStyle/index.android.js
@@ -7,8 +7,20 @@ import {
 } from 'react-native';
 
 const styles = {
-    styletext:{
-      fontSize: 20
+    styleText:{
+      fontSize: 30,
+      backgroundColor: '#08509B',
+      marginTop: 15,
+      color: '#fff',
+      fontWeight: '100',  //posso colocar um peso de 100 a 900
+      fontStyle: 'italic',
+      textAlign: 'center',
+      textDecorationLine: 'line-through' // Deixa sublinhado
+      /*Só funciona em IOS :-( 
+        shadowColor: '#000', //Defini a cor da sombra
+        shadowOffset: {width:-10, height:-15}, // Define a posição da sombra
+        shadowOpacity: 1 //Opacidade da sombra, forte ou fraca a visualização
+      */
     }
   }
 
@@ -16,7 +28,7 @@ export default class App extends Component {
   render() {
     return (
       <View>
-        <Text style={styles.styletext}>
+        <Text style={styles.styleText}>
           Frases do Dia!
         </Text>
       </View>
