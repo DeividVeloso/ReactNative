b0a65eae99a9ea6211042581aa20ea7098ffa04c
Deivid Veloso <veloso.deivid@gmail.com>
2017-06-02 09:11:21 -0300

    flexbox row column

--- appStyle/index.android.js
+++ appStyle/index.android.js
@@ -7,30 +7,34 @@ import {
 } from 'react-native';
 
 const styles = {
-    styleText:{
-      fontSize: 30,
-      backgroundColor: '#08509B',
-      marginTop: 15,
-      color: '#fff',
-      fontWeight: '100',  //posso colocar um peso de 100 a 900
-      fontStyle: 'italic',
-      textAlign: 'center',
-      textDecorationLine: 'line-through' // Deixa sublinhado
-      /*Só funciona em IOS :-( 
-        shadowColor: '#000', //Defini a cor da sombra
-        shadowOffset: {width:-10, height:-15}, // Define a posição da sombra
-        shadowOpacity: 1 //Opacidade da sombra, forte ou fraca a visualização
-      */
-    }
+  styleText: {
+    fontSize: 40,
+    backgroundColor: '#08509B',
+    width: 60,
+    height: 60
+  },
+  styleTextTwo: {
+    fontSize: 40,
+    backgroundColor: '#2A48FA',
+    width: 60,
+    height: 60
+  },
+  styleView: {
+    backgroundColor: 'skyblue',
+    height: 300,
+    justifyContent: 'center', //FlexBox alinhamento na tela
+    alignItems: 'center',
+    flexDirection: 'row'
   }
+}
 
 export default class App extends Component {
   render() {
+    const { styleText, styleTextTwo, styleView } = styles;
     return (
-      <View>
-        <Text style={styles.styleText}>
-          Frases do Dia!
-        </Text>
+      <View style={styleView}>
+        <Text style={styleText}>A</Text>
+        <Text style={styleTextTwo}>B</Text>
       </View>
     );
   }
