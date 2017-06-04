865233f4fbfa952a817b72ec7307ffd97b22e8c1
Deivid Veloso <Deivid Veloso>
2017-06-03 22:10:11 -0300

    add button TouchableOpacity

--- appStyle/index.android.js
+++ appStyle/index.android.js
@@ -4,14 +4,28 @@ import {
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  TouchableOpacity
 } from 'react-native';
 
 const styles = {
     main:{
-      backgroundColor: 'cornflowerblue',
-      padding: 40
+      paddingTop:40
+    },
+    buttonStyle:{
+      backgroundColor: '#48BBEC',
+      padding: 10,
+      borderColor:'#1D8EB8',
+      borderWidth: 2,
+      borderRadius: 8
+    },
+    textStyle:{
+      color: '#fff',
+      fontSize: 16,
+      fontWeight: 'bold',
+      alignSelf: 'center'
     }
+
 }
 
 const botaoPressionado = () =>{
@@ -20,15 +34,20 @@ const botaoPressionado = () =>{
 
 export default class App extends Component {
   render() {
-    const { main, contentOne, contentTwo, contentThree } = styles;
+    const { main, buttonStyle, textStyle} = styles;
     return (
       <View style={main}>
-        <Button 
+
+        <TouchableOpacity style={buttonStyle} onPress={botaoPressionado}>
+            <Text style={textStyle}>Clique aqui</Text>
+        </TouchableOpacity>
+
+        {/*<Button 
           onPress={botaoPressionado}
           title="Clique aqui"
           color="#841584"
           accessibilityLabel="Clique aqui para ler as noticias"// usado para deficiênte visual conseguir ler a tela e saber que aqui tem um botão
-        />
+        />*/}
       </View>
     );
   }
