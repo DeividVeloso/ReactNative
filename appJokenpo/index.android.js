556b65bf6bbe1eb024ffdd58a74a2a7407035697
Deivid Veloso <veloso.deivid@gmail.com>
2017-06-07 09:38:14 -0300

    finish coding JokenPo wit dynamic images

--- appJokenpo/index.android.js
+++ appJokenpo/index.android.js
@@ -13,7 +13,12 @@ class appJokenpo extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { escolhaUsuario: '', escolhaComputador: '', resultado : '' }
+    this.state = {
+      escolhaUsuario: '',
+      escolhaComputador: '',
+      resultado: '',
+      colorResult: false
+    }
   }
 
   jokenpo(escolhaUsuario) {
@@ -21,7 +26,7 @@ class appJokenpo extends Component {
     var numRandom = Math.floor(Math.random() * 3);
     switch (numRandom) {
       case 0:
-       escolhaComputador = 'pedra';
+        escolhaComputador = 'pedra';
         break;
       case 1:
         escolhaComputador = 'papel';
@@ -32,19 +37,27 @@ class appJokenpo extends Component {
     }
 
 
-    var resultado = ''
+    var resultado = '';
+    var colorResult = false;
 
-    if(escolhaComputador == escolhaUsuario){
-        resultado = 'Empate!'
-    } else if((escolhaComputador == 'pedra' && escolhaUsuario == 'papel') 
-              || (escolhaComputador == 'papel' && escolhaUsuario == 'tesoura') 
-              || (escolhaComputador == 'tesoura' && escolhaUsuario == 'pedra')){
-             resultado = 'Usuário ganhou!'
-    }else {
-        resultado = 'Computador ganhou!'
+    if (escolhaComputador == escolhaUsuario) {
+      resultado = 'Empate!'
+    } else if ((escolhaComputador == 'pedra' && escolhaUsuario == 'papel')
+      || (escolhaComputador == 'papel' && escolhaUsuario == 'tesoura')
+      || (escolhaComputador == 'tesoura' && escolhaUsuario == 'pedra')) {
+      resultado = 'Você ganhou!';
+      colorResult = true;
+    } else {
+      resultado = 'Máquina ganhou!'
     }
 
-    this.setState({ escolhaUsuario: escolhaUsuario, escolhaComputador: escolhaComputador, resultado: resultado });
+    this.setState({ escolhaUsuario: escolhaUsuario, escolhaComputador: escolhaComputador, resultado: resultado, colorResult: colorResult });
+  }
+
+  selectColor() {
+    return {
+      color: this.state.colorResult ? 'green' : 'red'
+    }
   }
 
   render() {
@@ -52,47 +65,82 @@ class appJokenpo extends Component {
       <View>
         <Topo></Topo>
         <View style={styles.painelBotoes}>
-            <View style={ styles.botaoEscolha }>
-              <Button title='pedra' onPress={() => this.jokenpo('pedra')}></Button>
-            </View>
-            <View style={ styles.botaoEscolha }>
-              <Button title='papel' onPress={() => this.jokenpo('papel')}></Button>
-            </View>
-            <View style={ styles.botaoEscolha }>
-              <Button title='tesoura' onPress={() => this.jokenpo('tesoura')}></Button>
-            </View>
+          <View style={styles.botaoEscolha}>
+            <Button title='pedra' onPress={() => this.jokenpo('pedra')}></Button>
+          </View>
+          <View style={styles.botaoEscolha}>
+            <Button title='papel' onPress={() => this.jokenpo('papel')}></Button>
+          </View>
+          <View style={styles.botaoEscolha}>
+            <Button title='tesoura' onPress={() => this.jokenpo('tesoura')}></Button>
+          </View>
         </View>
-        <View>
-          <Text>Escolha do computador: {this.state.escolhaComputador}</Text>
-          <Text>Escolha do usuário: {this.state.escolhaUsuario}</Text>
-          <Text>Resultado: {this.state.resultado}</Text>
+        <View style={styles.painelResult}>
+          <Text style={[styles.txtResultado, this.selectColor()]}> {this.state.resultado}</Text>
+          
+          <Icone  escolha={this.state.escolhaComputador} jogador='Computador: '></Icone>
+          <Icone  escolha={this.state.escolhaUsuario} jogador='Você: '></Icone>
+
         </View>
-        
-        
       </View>
     );
   }
 }
 
- class Topo extends Component{
-    render(){
-        return(
-            <View>
-                <Image source={require('./imgs/jokenpo.png')}></Image>
-            </View>
-        );
+class Icone extends Component{
+  render(){
+    if(this.props.escolha){
+      return(
+        <View style={styles.icone}>
+            <Text style={styles.txtJogador}>{this.props.jogador}</Text>
+            { 
+              this.props.escolha == 'pedra' ? <Image source={require(`./imgs/pedra.png`)}></Image> 
+              : this.props.escolha == 'tesoura' ?  <Image source={require(`./imgs/tesoura.png`)}></Image>
+              : this.props.escolha == 'papel' ?  <Image source={require(`./imgs/papel.png`)}></Image>
+              : <Image></Image>
+            }
+        </View>
+      )
+    }else{
+      return false;
     }
+  }
+}
+
+class Topo extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('./imgs/jokenpo.png')}></Image>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-   botaoEscolha:{
-     width: 90
-   },
-   painelBotoes:{
-     flexDirection: 'row', //Alinha os componetes em linha(display-inline) ao invés de display block
-     justifyContent: 'space-between',//dá espaçamento entre eles com a mesma proporção
-     margin: 10,
-   }
+  botaoEscolha: {
+    width: 90
+  },
+  painelBotoes: {
+    flexDirection: 'row', //Alinha os componetes em linha(display-inline) ao invés de display block
+    justifyContent: 'space-between',//dá espaçamento entre eles com a mesma proporção
+    margin: 10,
+  },
+  painelResult: {
+    alignItems: 'center'
+  },
+  txtResultado: {
+    marginBottom: 10,
+    fontSize: 25,
+    fontWeight: 'bold' 
+  },
+  icone:{
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  txtJogador:{
+    fontSize: 20
+  }
 });
 
 AppRegistry.registerComponent('appJokenpo', () => appJokenpo);
