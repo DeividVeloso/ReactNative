bd618756b8c8d406ed7c729301725c69db324306
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-06-20 10:36:53 -0300

    learning about mount cycle of React component

--- appUsingAxios/src/components/ListItens.js
+++ appUsingAxios/src/components/ListItens.js
@@ -2,8 +2,21 @@ import React, { Component } from "react";
 import { Text, View } from "react-native";
 
 export default class ListItens extends Component {
+  constructor(props) {
+    super(props);
+    console.log("Construindo a aplicação");
+  }
+
+  componentWillMount() {
+    console.log("faz algo antes de renderizar o componente na tela");
+  }
+
+  componentDidMount() {
+    console.log("faz algo depois de renderizar o componente na tela");
+  }
+
   render() {
-      console.log('Valor dentro do app no console.log()')
+    console.log("Componente renderizou");
     return (
       <View>
         <Text> Teste </Text>
