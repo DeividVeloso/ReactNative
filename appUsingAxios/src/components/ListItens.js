e78dc81e1f22b20b27c3fe8ddcf9bfe7a351ecc4
Deivid Veloso <veloso.deivid@gmail.com>
2017-06-20 19:43:29 -0300

    using map function to render elements in screen

--- appUsingAxios/src/components/ListItens.js
+++ appUsingAxios/src/components/ListItens.js
@@ -1,24 +1,29 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import Axios from 'axios';
 
 import Item from './Item';
 
 const urlApi = 'http://faus.com.br/recursos/c/dmairr/api/itens.html';
 export default class ListItens extends Component {
+  constructor(props) {
+    super(props);
 
-componentWillMount() {
+    this.state = { listItens: [] };
+  }
+
+  componentWillMount() {
     Axios.get(urlApi)
-    .then((response) => { console.log(response); })
-    .catch((error) => console.log(error));
-}
+      .then(response => {
+        this.setState({ listItens: response.data });
+      })
+      .catch(error => error);
+  }
 
   render() {
     return (
       <View>
-        <Item />
-        <Item />
-        <Item />
+        {this.state.listItens.map((item, index) => (<Text key={index}> {item.titulo} </Text>))}
       </View>
     );
   }
