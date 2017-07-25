be371f093bb89c719ae328bba9a995a7499c76dc
Deivid Veloso <veloso.deivid@gmail.com>
2017-07-25 09:30:52 -0300

    add Navigator

--- appNavegandoTelas/index.android.js
+++ appNavegandoTelas/index.android.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import {
   AppRegistry,
-  View,
-  StatusBar
+  Navigator
 } from 'react-native';
 
 import CenaPrincipal from './src/components/CenaPrincipal'
+import CenaClientes from './src/components/CenaClientes'
 
 export default class appNavegandoTelas extends Component {
   render() {
     return (
-      <View>
-        <CenaPrincipal/>
-      </View>
+        <Navigator/>
     );
   }
 }
---
+++ appNavegandoTelas/src/components/CenaClientes.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  View,
+  Text,
+  StatusBar,
+  Image,
+  StyleSheet
+} from "react-native";
+
+import BarraNavegacao from "./BarraNavegacao";
+
+const  detalheClientes = require('../imgs/detalhe_cliente.png');
+const  cliente1 = require('../imgs/cliente1.png');
+const  cliente2 = require('../imgs/cliente2.png');
+
+export default class CenaClientes extends Component {
+  render() {
+    return (
+      <View>
+        <StatusBar
+          hidden={false} //Usado para esconder a barra de status
+          backgroundColor={"#B9C941"}
+        />
+        <BarraNavegacao />
+        <View style={styles.detalhes}>
+            <Image source={detalheClientes} />
+            <Text style={styles.txtTitulo}>Nossos Clientes</Text>
+        </View>
+         <View style={styles.detalheClientes}>
+            <Image source={cliente1} />
+            <Text style={styles.txtDetalheCliente}>Lorem ipsum dolorem</Text>
+        </View>
+         <View style={styles.detalheClientes}>
+            <Image source={cliente2} />
+            <Text style={styles.txtDetalheCliente}>Lorem ipsum dolorem</Text>
+         </View>
+       </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+    detalhes:{
+        flexDirection: 'row',
+        marginTop: 20
+    },
+    txtTitulo:{
+        fontSize: 30,
+        color: '#B9C941',
+        marginLeft: 10,
+        marginTop: 25
+    },
+    detalheClientes : {
+       padding:20,
+       marginTop: 10
+    },
+    txtDetalheCliente:{
+        fontSize:18,
+        marginLeft: 10
+    }
+    
+})
