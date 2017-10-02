cdc75fcec248a4dfc00cc7cb27ed04ef38a6afee
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-10-02 09:58:48 -0300

    add tabnavibar

--- pocs/index.android.js
+++ pocs/index.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { AppRegistry, Text, View, Button } from "react-native";
+import { AppRegistry, Text, View, Button, Image  } from "react-native";
 //import { TabNavigator, TabBarBottom } from "react-navigation";
 
 import Container from "./src/components/Container";
@@ -14,6 +14,11 @@ const screens =  {
   Chat,
   Menu
 };
+
+// const btnStart = require();
+// const btnDoubt = require();
+// const btnRole = require();
+// const btnMenu = require();
 
 const ViewFlex = (props) => <View style={{ flex: 1 }} {...props} />;
 
@@ -44,17 +49,56 @@ class TabNavigator extends Component {
             onLogout={this.onLogout}
           />
         </ViewFlex>
-        <View style={{ flexDirection: 'row' }}>
-          <Button onPress={() => this.changeScreen('Home')} title="home" />
-          <Button onPress={() => this.changeScreen('Chat')} title="chat" />
-          <Button onPress={() => this.changeScreen('Menu')} title="menu" />
-        </View>
+        <ViewFlex style={{ flexDirection: 'row' }}>
+          <Button  onPress={() => this.changeScreen('Home')} title="home"></Button>
+          <Button  onPress={() => this.changeScreen('Chat')} title="chat"></Button>
+          <Button  onPress={() => this.changeScreen('Menu')} title="menu"></Button>
+        </ViewFlex>
       </ViewFlex>
     )
   }
 }
 
 AppRegistry.registerComponent("pocs", () => TabNavigator);
+
+
+// class TabNavigator extends Component {
+//   state = {
+//     loggedIn: false,
+//     currentScreen: 'Home'
+//   };
+
+//   changeScreen = (key) => this.setState({ currentScreen: key });
+
+//   onLogin = () => this.setState({ loggedIn: true });
+
+//   onLogout = () => this.setState({ loggedIn: false });
+
+//   render() {
+//     if (!this.state.loggedIn) {
+//       return <Login onLogin={this.onLogin} />;
+//     }
+    
+//     const Screen = screens[this.state.currentScreen];
+
+//     return (
+//       <ViewFlex>
+//         <ViewFlex>
+//           <Screen
+//             navigation={{ navigate: this.changeScreen }}
+//             onLogout={this.onLogout}
+//           />
+//         </ViewFlex>
+//         <ViewFlex style={{ flexDirection: 'row' }}>
+//           <TouchableOpacity  onPress={() => this.changeScreen('Home')} title="home"></TouchableOpacity>
+//           <TouchableOpacity  onPress={() => this.changeScreen('Chat')} title="chat"></TouchableOpacity>
+//           <TouchableOpacity  onPress={() => this.changeScreen('Menu')} title="menu"></TouchableOpacity>
+//         </ViewFlex>
+//       </ViewFlex>
+//     )
+//   }
+// }
+
 
 // const pocs = TabNavigator(
 //   {
