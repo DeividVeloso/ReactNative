2d888d58250c2dfe704988223ac355dd0f5e56c2
Deivid Higino Veloso <dveloso@acessocard.com.br>
2017-10-04 10:56:01 -0300

    add camera

--- pocs/android/app/src/main/java/com/pocs/MainApplication.java
+++ pocs/android/app/src/main/java/com/pocs/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 import com.centaurwarchief.smslistener.SmsListenerPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -24,7 +25,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-          new SmsListenerPackage()
+          new SmsListenerPackage(),
+          new RCTCameraPackage()
       );
     }
   };
