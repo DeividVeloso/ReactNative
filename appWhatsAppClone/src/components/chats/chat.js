ca31de283b35976547d7f5bf2c704f64589c4aa1
Deivid Veloso <veloso.deivid@gmail.com>
2017-12-16 15:48:02 -0200

    add chat screen with styles

--- appWhatsAppClone/src/components/chats/chat.js
+++ appWhatsAppClone/src/components/chats/chat.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 
 class Chat extends Component {
   render() {
     return (
-      <View style={{ marginTop: 80 }}>
-        <Text>Conversa</Text>
+      <View
+        style={{
+          flex: 1,
+          marginTop: 50,
+          backgroundColor: '#eee4dc',
+          padding: 10,
+        }}
+      >
+        <View style={{ flex: 1, paddingBottom: 20 }} />
+        <View style={{ flexDirection: 'row', height: 60 }}>
+          <TextInput style={{ flex: 4, backgroundColor: '#fff', fontSize: 18 }} />
+          <TouchableOpacity onPress={() => false}>
+            <Image source={require('../../imgs/enviar_mensagem.png')} />
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
